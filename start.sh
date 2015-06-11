e0bd44d01b8033032d3aab140df48e31cfea1349
zcsoft <zcsoft@icloud.com>
2015-06-11 15:51:06 +0800

    123

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+export LC_APP_ID='3evt31e7h9uuwyrl4ac3kojxyojch8ll2ixo909jnect184h'
+export LC_APP_KEY='jizrvvg0ai949i4e1p3w1dwv5gm4vs6b27f0ubcqrx45q98g'
+export LC_APP_MASTER_KEY='wyy9pfyxhtya1xaotfw9pad0r132xolebi9b01myk5flw6mp'
+export LC_APP_PORT=3000
+
+python wsgi.py
